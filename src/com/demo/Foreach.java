f861f877125adc07f3ae9aea9eaa6469f3974ed1
Achsaya <achsaya.912@gmail.com>
2023-01-31 18:36:45 +0530

    committing this file

---
+++ src/com/demo/Foreach.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+public class Foreach {
+    public static void main(String[] args) {
+        int [] marks = {10,20,30,40,50};
+        for(int num : marks){
+            System.out.println(num);
+        }
+    }
+}
