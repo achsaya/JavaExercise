e272541449d85cd1e3f193a2b202319b22382bd9
Achsaya <achsaya.912@gmail.com>
2023-01-31 12:56:28 +0530

    committing this file

--- src/com/demo/EmployeeTest.java
+++ src/com/demo/EmployeeTest.java
@@ -6,6 +6,7 @@ public class EmployeeTest {
         /* Create two objects using constructor */
         Employee empOne = new Employee("James Smith");
         Employee empTwo = new Employee("Mary Anne");
+        Employee empThree = new Employee("Ani");
 
         // Invoking methods for each object created
         empOne.empAge(26);
@@ -17,5 +18,12 @@ public class EmployeeTest {
         empTwo.empDesignation("Software Engineer");
         /*empTwo.empSalary(500);*/
         empTwo.printEmployee();
+
+        empThree.empAge(21);
+        empThree.empDesignation("Data Engineer");
+        /*empTwo.empSalary(500);*/
+        empThree.printEmployee();
+
+
     }
 }
